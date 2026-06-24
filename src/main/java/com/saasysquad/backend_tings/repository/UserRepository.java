44a3b758e84ee4425e37233d5ebbbe4831dfcdf6
Jason Li <z5481421@ad.unsw.edu.au>
2026-06-24 23:24:24 +1000

    Added dotenv dependency

--- src/main/java/com/saasysquad/backend_tings/repository/UserRepository.java
+++ src/main/java/com/saasysquad/backend_tings/repository/UserRepository.java
@@ -1,8 +1,32 @@
 package com.saasysquad.backend_tings.repository;
 
+import com.saasysquad.backend_tings.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
 
 @Repository
 public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
 
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public User create(User user) {
+        String sql = "insert into users " +
+                    "(user_id, user_name, email, password_hash, created_at) " +
+                    "values " +
+                    "(?, ?, ? ,? ,?)";
+
+        jdbcTemplate.update(sql,
+                            user.getId(),
+                            user.getUsername(),
+                            user.getEmail(),
+                            user.getPasswordHash(),
+                            Instant.now().toString());
+
+        return user;
+    }
 }
